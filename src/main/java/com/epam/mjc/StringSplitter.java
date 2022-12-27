e55105e317e8360014effacd661f66653c11756e
Yuliya Raikova <yuradev2001@gmail.com>
2022-12-27 10:36:45 +0300

    .

--- src/main/java/com/epam/mjc/StringSplitter.java
+++ src/main/java/com/epam/mjc/StringSplitter.java
@@ -1,6 +1,7 @@
 package com.epam.mjc;
 
 import java.util.*;
+import java.util.stream.Collectors;
 
 public class StringSplitter {
 
@@ -12,57 +13,17 @@ public class StringSplitter {
      * @return List of substrings
      */
     public List<String> splitByDelimiters(String source, Collection<String> delimiters) {
-        StringBuilder stringBuilder1 = new StringBuilder(source);
-        String[] delimits = new String[delimiters.size()];
-        delimiters.toArray(delimits);
-        LinkedList<String> list = new LinkedList<>();
-        while (stringBuilder1.length() > 0) {
-            String sub = "";
-            int lengthOfMinimal = stringBuilder1.length();
-            int ind = 0;
-            for (int i = 0; i < delimiters.size(); i++) {
-                String delim = delimits[i];
-                try {
-                    String subStr = new StringTokenizer(stringBuilder1.toString(), delim, false).nextToken();
-                    if (subStr.length() <= lengthOfMinimal && subStr.length() > 0 && !subStr.equals(delim)) {
-                        boolean flag = false;
-                        for (String deli : delimits) {
-                            if (subStr.contains(deli)) {
-                                flag = true;
-                                break;
-                            }
-                        }
-
-                        if (flag) {
-                            continue;
-                        }
-                        sub = subStr;
-                        ind = i;
-                        lengthOfMinimal = subStr.length();
-                    }
-                }
-                catch (NoSuchElementException exc) {
-                    sub = stringBuilder1.toString();
-                    boolean flag1 = false;
-                    for (String deli : delimits) {
-                        if (sub.contains(deli)) {
-                            flag1 = true;
-                            break;
-                        }
-                    }
-
-                    if (flag1) {
-                        sub = "";
-                    }
-                }
-            }
-
-            if (sub.length() > 0) {
-                list.add(sub);
-            }
-            stringBuilder1 = stringBuilder1.delete(0, sub.length() + delimits[ind].length());
+        StringBuilder regex = new StringBuilder("[");
+        for (String delim : delimiters) {
+            regex.append("(");
+            regex.append(delim);
+            regex.append(")");
         }
 
-        return list;
+        regex.append("]+");
+
+        String[] substrings = source.split(regex.toString());
+
+        return Arrays.stream(substrings).filter(str -> (!str.isEmpty() && !str.isBlank())).collect(Collectors.toList());
     }
 }
